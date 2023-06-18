8606670c934f2cb629097abfd677c10a5e5f3a29
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-06-18 17:56:08 +0330

    Animations CSS

--- Navigation Nation/script.js
+++ Navigation Nation/script.js
@@ -15,10 +15,32 @@ function toggleNav() {
     // Animate In - Overlay
     overlay.classList.remove("overlay-slide-left");
     overlay.classList.add("overlay-slide-right");
+    // Animate In - Nav Items
+    nav1.classList.remove("slide-out-1");
+    nav1.classList.add("slide-in-1");
+    nav2.classList.remove("slide-out-2");
+    nav2.classList.add("slide-in-2");
+    nav3.classList.remove("slide-out-3");
+    nav3.classList.add("slide-in-3");
+    nav4.classList.remove("slide-out-4");
+    nav4.classList.add("slide-in-4");
+    nav5.classList.remove("slide-out-5");
+    nav5.classList.add("slide-in-5");
   } else {
     // Animate Out - Overlay
     overlay.classList.remove("overlay-slide-right");
     overlay.classList.add("overlay-slide-left");
+    // Animate Out - Nav Items
+    nav1.classList.remove("slide-in-1");
+    nav1.classList.add("slide-out-1");
+    nav2.classList.remove("slide-in-2");
+    nav2.classList.add("slide-out-2");
+    nav3.classList.remove("slide-in-3");
+    nav3.classList.add("slide-out-3");
+    nav4.classList.remove("slide-in-4");
+    nav4.classList.add("slide-out-4");
+    nav5.classList.remove("slide-in-5");
+    nav5.classList.add("slide-out-5");
   }
 }
 
