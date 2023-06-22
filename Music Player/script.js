8d4f89a9b990046dcab0e4e2cfcf3c9c1352b275
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-06-22 16:12:26 +0330

    Progress Bar

--- Music Player/script.js
+++ Music Player/script.js
@@ -2,6 +2,8 @@ const image = document.querySelector("img");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
@@ -87,6 +89,17 @@ function nextSong() {
 // On Load - Select First Song
 loadSong(songs[songIndex]);
 
+// Update Progress Bar & Time
+function updateProgressBar(e) {
+  if (isPlaing) {
+    const { duration, currentTime } = e.srcElement;
+    // Update Progress Bar Width
+    const progressPrecent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPrecent}%`;
+  }
+}
+
 // Event Listeners
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
