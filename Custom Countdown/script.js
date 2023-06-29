185f760f89cd3953b7f68819a6f3a699fc159ad6
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-06-29 11:48:27 +0330

    Form Values

--- Custom Countdown/script.js
+++ Custom Countdown/script.js
@@ -2,6 +2,20 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+let countdownTitle = "";
+let countdownDate = "";
+
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
+
+// Take Values from Form Input
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  console.log(countdownTitle, countdownDate);
+}
+
+// Event Listeners
+countdownForm.addEventListener("submit", updateCountdown);
