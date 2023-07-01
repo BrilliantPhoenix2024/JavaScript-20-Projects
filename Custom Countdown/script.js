3060f068051f4dc929e143fdf8637592e1ca4895
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-07-02 00:29:03 +0330

    Code Review

--- Custom Countdown/script.js
+++ Custom Countdown/script.js
@@ -13,7 +13,7 @@ const completeBtn = document.getElementById("complete-button");
 
 let countdownTitle = "";
 let countdownDate = "";
-let countdownValue = Date;
+let countdownValue = new Date();
 let countdownActive;
 let savedCountdown;
 
