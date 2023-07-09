70a945a93dcf26373250d85f2de6dd3e9d7b93c4
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-07-09 17:25:40 +0330

    JS - Playback Seed

--- Video Player/script.js
+++ Video Player/script.js
@@ -8,6 +8,7 @@ const volumeBar = document.querySelector(".volume-bar");
 const currentTime = document.querySelector(".time-elapsed");
 const duration = document.querySelector(".time-duration");
 const fullscreenBtn = document.querySelector(".fullscreen");
+const speed = document.querySelector(".player-speed");
 
 // Play & Pause ----------------------------------- //
 function showPlayIcon() {
@@ -97,6 +98,9 @@ function toggleMute() {
 }
 
 // Change Playback Speed -------------------- //
+function changeSpeed() {
+  video.playbackRate = speed.value;
+}
 
 // Fullscreen ------------------------------- //
 
@@ -108,3 +112,4 @@ video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeValume);
 volumeIcon.addEventListener("click", toggleMute);
+speed.addEventListener("change", changeSpeed);
