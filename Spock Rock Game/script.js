8ff3b7a3ff520679b4e1c49143c41cb39e9439e7
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-07-16 09:56:36 +0330

    JS - Confetti Functionality

--- Spock Rock Game/script.js
+++ Spock Rock Game/script.js
@@ -1,3 +1,5 @@
+import { removeConfetti, startConfetti, stopConfetti } from "./confetti";
+
 const playerScoreEl = document.getElementById("playerScore");
 const playerChoiceEl = document.getElementById("playerChoice");
 const computerScoreEl = document.getElementById("computerScore");
@@ -35,6 +37,8 @@ function resetSelected() {
   allGameIcons.forEach((icon) => {
     icon.classList.remove("selected");
   });
+  stopConfetti();
+  removeConfetti();
 }
 
 // Reset Score & playerChoice / computerChoice
@@ -100,6 +104,7 @@ function updateScore(playerChoice) {
   } else {
     const choice = choices[playerChoice];
     if (choice.defeats.indexOf(computerChoice) > -1) {
+      startConfetti();
       resetSelected.textContent = "You Won!";
       playerScoreNumber++;
       playerScoreEl.textContent = playerScoreNumber;
