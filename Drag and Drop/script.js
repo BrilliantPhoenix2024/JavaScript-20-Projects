085fee9b19ad2d30c7db7b44f42c973a0151a4c7
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-07-28 22:07:36 +0330

    Code Review

--- Drag and Drop/script.js
+++ Drag and Drop/script.js
@@ -157,22 +157,22 @@ function hideInputBox(column) {
 
 // Allows arrays to reflect Drag and Drop items
 function rebuiltArrays() {
-  backlogListArray = [];
-  for (let i = 0; i < backlogList.children.length; i++) {
-    backlogListArray.push(backlogList.children[i].textContent);
-  }
-  progressListArray = [];
-  for (let i = 0; i < progressList.children.length; i++) {
-    progressListArray.push(progressList.children[i].textContent);
-  }
-  completeListArray = [];
-  for (let i = 0; i < completeList.children.length; i++) {
-    completeListArray.push(completeList.children[i].textContent);
-  }
-  onHoldListArray = [];
-  for (let i = 0; i < onHoldList.children.length; i++) {
-    onHoldListArray.push(onHoldList.children[i].textContent);
-  }
+  backlogListArray = Array.from(backlogList.children).map(
+    (item) => item.textContent
+  );
+
+  progressListArray = Array.from(progressList.children).map(
+    (item) => item.textContent
+  );
+
+  completeListArray = Array.from(completeList.children).map(
+    (item) => item.textContent
+  );
+
+  onHoldListArray = Array.from(onHoldList.children).map(
+    (item) => item.textContent
+  );
+
   updateDOM();
 }
 
