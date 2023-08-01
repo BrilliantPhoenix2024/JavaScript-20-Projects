92c279e91e2dd9b8a98b32e58d439dd744462cde
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2023-08-01 19:46:11 +0330

    JS - Reset Value, Add Decimal

--- Calculator/script.js
+++ Calculator/script.js
@@ -9,6 +9,13 @@ function sendNumberValue(number) {
     displayValue === "0" ? number : displayValue + number;
 }
 
+function addDecimal() {
+  // If no decimal, add one
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
+}
+
 // Add Event Listeners for numbers, operator, decimal buttons
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
@@ -16,6 +23,14 @@ inputBtns.forEach((inputBtn) => {
   } else if (inputBtn.classList.contains("operator")) {
     inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains("decimal")) {
-    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+    inputBtn.addEventListener("click", () => addDecimal());
   }
 });
+
+// Reset display
+function resetAll() {
+  calculatorDisplay.textContent = "0";
+}
+
+// Event Listener
+clearBtn.addEventListener("click", resetAll);
